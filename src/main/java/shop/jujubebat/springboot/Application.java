3bfa1e1aaa4a9dc3aefef8b703710c23e8c345d2
jujubebat <bbwwpark@naver.com>
2020-09-08 15:45:24 +0900

    사용자 인증이 필요한 api 테스트 코드 구현 완료.

--- src/main/java/shop/jujubebat/springboot/Application.java
+++ src/main/java/shop/jujubebat/springboot/Application.java
@@ -2,9 +2,7 @@ package shop.jujubebat.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication // 스프링 부트 설정, 빈 등록 자동 설정
 public class Application {
     public static void main(String[] args) {
--- src/test/java/shop/jujubebat/springboot/web/PostsApiContollerTest.java
+++ src/test/java/shop/jujubebat/springboot/web/PostsApiContollerTest.java
@@ -1,17 +1,21 @@
 package shop.jujubebat.springboot.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import shop.jujubebat.springboot.domain.posts.Posts;
 import shop.jujubebat.springboot.domain.posts.PostsRepository;
 import shop.jujubebat.springboot.web.dto.PostsSaveRequestDto;
@@ -21,6 +25,10 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -36,12 +44,26 @@ public class PostsApiContollerTest {
     @Autowired
     private PostsRepository postsRepository;
 
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup(){
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
     @After
     public void tearDown() throws Exception {
         postsRepository.deleteAll();
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void Posts_등록_테스트() throws Exception {
         // given
         String title = "title";
@@ -54,19 +76,21 @@ public class PostsApiContollerTest {
 
         String url = "http://localhost:" + port + "/api/v1/posts";
 
+
         // when
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class); // (url, 요청데이터, 반환타입)
+        mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
         // then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK); // 응답 코드가 200인지 테스트
-        assertThat(responseEntity.getBody()).isGreaterThan(0L); // 응답값이 0보다 큰지 확인(지금 테스트하려는 api는 데이터 저장을 성공하면 PK를 return 해줌)
-
-        List<Posts> all = postsRepository.findAll(); // 실제로 잘 저장되어있는지 확인.
-        assertThat(all.get(0).getTitle()).isEqualTo(title); // db에서 데이터를 select한다음, given과 일치하는지 테스트
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void Posts_수정_테스트() throws Exception {
         // given
         Posts savedPosts = postsRepository.save(Posts.builder()
@@ -88,12 +112,13 @@ public class PostsApiContollerTest {
 
         HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
 
-        // when
-        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+       // when
+        mvc.perform(put(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
         // then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
         assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
